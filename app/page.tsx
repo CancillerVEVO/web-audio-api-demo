86f57df0c352ee8649f05f43fb9af01441704f29
cancillerVEVO <emma.lopez.ojeda@gmail.com>
2025-12-25 21:38:59 -0800

    demo: Using the Web Audio API

--- app/page.tsx
+++ app/page.tsx
@@ -1,65 +1,110 @@
-import Image from "next/image";
+"use client";
+
+import { useEffect, useMemo, useRef, useState } from "react";
 
 export default function Home() {
+  const src = "/sounds/kintsugi.mp3";
+
+  const audioContext = useMemo(() => new AudioContext(), []);
+
+  const audioRef = useRef<HTMLAudioElement | null>(null);
+  const gainNodeRef = useRef<GainNode | null>(null);
+  const pannerNodeRef = useRef<StereoPannerNode | null>(null);
+
+  const [gain, setGain] = useState<number>(1);
+  const [pan, setPan] = useState<number>(0);
+
+  useEffect(() => {
+    if (!audioRef.current) return;
+
+    if (!gainNodeRef.current) {
+      gainNodeRef.current = audioContext.createGain();
+      gainNodeRef.current.gain.value = gain;
+    }
+
+    if (!pannerNodeRef.current) {
+      const pannerOptions: StereoPannerOptions = {
+        pan,
+      };
+      pannerNodeRef.current = new StereoPannerNode(audioContext, pannerOptions);
+    }
+
+    const track = audioContext.createMediaElementSource(audioRef.current);
+
+    track
+      .connect(gainNodeRef.current)
+      .connect(pannerNodeRef.current)
+      .connect(audioContext.destination);
+
+    return () => {
+      track.disconnect();
+    };
+  }, [audioContext, gain, pan]);
+
+  const handleGainChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const value = parseFloat(e.target.value);
+
+    setGain(value);
+
+    if (!gainNodeRef.current) return;
+
+    gainNodeRef.current.gain.setValueAtTime(value, audioContext.currentTime);
+  };
+
+  const handlePanChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const value = parseFloat(e.target.value);
+
+    setPan(value);
+
+    if (!pannerNodeRef.current) return;
+
+    pannerNodeRef.current.pan.setValueAtTime(value, audioContext.currentTime);
+  };
+
+  const togglePlay = async () => {
+    if (!audioRef.current) return;
+
+    if (audioContext.state === "suspended") {
+      await audioContext.resume();
+    }
+
+    if (audioRef.current.paused) {
+      audioRef.current.play();
+    } else {
+      audioRef.current.pause();
+    }
+  };
+
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
+    <div>
+      <h1>Web Audio API</h1>
+
+      <audio ref={audioRef} src={src} />
+
+      <label>
+        Gain: {gain.toFixed(2)}
+        <input
+          type="range"
+          min={0}
+          max={2}
+          step={0.01}
+          value={gain}
+          onChange={handleGainChange}
         />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
+      </label>
+      <label>
+        Pan: {pan.toFixed(2)}
+        <input
+          type="range"
+          min={-1}
+          max={1}
+          value={pan}
+          step={0.01}
+          onChange={handlePanChange}
+        />
+      </label>
+
+      <button onClick={togglePlay}>Play / Pause</button>
     </div>
   );
 }
