f1a4f5feeae92a4093202fcfa15e4196c4b350dd
cancillerVEVO <emma.lopez.ojeda@gmail.com>
2026-01-06 14:57:47 -0600

    frequency range

--- app/examples/components/Pad.tsx
+++ app/examples/components/Pad.tsx
@@ -13,7 +13,7 @@ export default function Pad({ label, isChecked, onChange }: PadProps) {
         <input
           type="checkbox"
           checked={isChecked}
-          className="appearance-none size-20 bg-sky-700 border-solid border-2 border-sky-900 rounded-md checked:bg-sky-300 checked:border-sky-500 active:scale-95 transition-transform duration-100 ml-2"
+          className="appearance-none size-20 border-solid border-2 border-sky-900 rounded-md checked:bg-sky-300 checked:border-sky-500 active:scale-95 transition-transform duration-100 ml-2"
           onChange={onChange}
         />
       </label>
--- app/examples/page.tsx
+++ app/examples/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { useEffect, useRef, useState } from "react";
+import { useRef, useState } from "react";
 import { WAVE_TABLE } from "./wavetable";
 import Pad from "./components/Pad";
 
@@ -15,35 +15,23 @@ type NoteQueueItem = {
 const EMPTY_BAR: boolean[] = [false, false, false, false];
 
 export default function Page() {
-  const audioContextRef = useRef<AudioContext | null>(null);
-  const waveRef = useRef<PeriodicWave | null>(null);
-
   const [attackTime, setAttackTime] = useState<number>(0.2);
   const [releaseTime, setReleaseTime] = useState<number>(0.5);
+  const [frequency, setFrequency] = useState<number>(100);
   const [tempo, setTempo] = useState<number>(60);
+  const [sweepBar, setSweepBar] = useState<boolean[]>(EMPTY_BAR);
+  const [isPlaying, setIsPlaying] = useState<boolean>(false);
+
+  const audioContextRef = useRef<AudioContext | null>(null);
+  const waveRef = useRef<PeriodicWave | null>(null);
   const tempoRef = useRef(tempo);
   const currentNoteRef = useRef(0);
   const nextNoteTimeRef = useRef(0);
-  const [isPlaying, setIsPlaying] = useState<boolean>(false);
   const notesInQueue = useRef<NoteQueueItem[]>([]);
-
   const timerIDRef = useRef<ReturnType<typeof setTimeout> | null>(null);
-
   const attackRef = useRef(attackTime);
   const releaseRef = useRef(releaseTime);
-  const [sweepBar, setSweepBar] = useState<boolean[]>(EMPTY_BAR);
-
-  useEffect(() => {
-    tempoRef.current = tempo;
-  }, [tempo]);
-
-  useEffect(() => {
-    attackRef.current = attackTime;
-  }, [attackTime]);
-
-  useEffect(() => {
-    releaseRef.current = releaseTime;
-  }, [releaseTime]);
+  const frequencyRef = useRef(frequency);
 
   const playSweep = (time: number): void => {
     const audioCtx = audioContextRef.current;
@@ -52,7 +40,7 @@ export default function Page() {
     if (!audioCtx || !wave) return;
 
     const osc = new OscillatorNode(audioCtx, {
-      frequency: 380,
+      frequency: frequencyRef.current,
       type: "custom",
       periodicWave: wave,
     });
@@ -70,27 +58,6 @@ export default function Page() {
     osc.start(time);
     osc.stop(time + SWEEP_LENGTH);
   };
-
-  // TODO: Move to separate components
-  const handleAttackChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const value = parseFloat(e.target.value);
-
-    setAttackTime(value);
-  };
-
-  const handleReleaseChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const value = parseFloat(e.target.value);
-
-    setReleaseTime(value);
-  };
-
-  const handleBPMChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const value = parseFloat(e.target.value);
-
-    setTempo(value);
-  };
-
-  // TIME UTILITY FUNCTIONS
 
   const nextNote = () => {
     const secondsPerBeat = 60.0 / tempoRef.current;
@@ -123,6 +90,77 @@ export default function Page() {
 
     timerIDRef.current = setTimeout(scheduler, LOOKAHEAD);
   };
+  // event handlers
+  const handleAttackChange = (e: React.ChangeEvent<HTMLInputElement>): void => {
+    const value = parseFloat(e.target.value);
+
+    setAttackTime(value);
+    attackRef.current = value;
+  };
+
+  const handleReleaseChange = (
+    e: React.ChangeEvent<HTMLInputElement>
+  ): void => {
+    const value = parseFloat(e.target.value);
+
+    setReleaseTime(value);
+    releaseRef.current = value;
+  };
+
+  const handleBPMChange = (e: React.ChangeEvent<HTMLInputElement>): void => {
+    const value = parseFloat(e.target.value);
+
+    setTempo(value);
+    tempoRef.current = value;
+  };
+
+  const handleFrequencyChange = (
+    e: React.ChangeEvent<HTMLInputElement>
+  ): void => {
+    const value = parseFloat(e.target.value);
+
+    setFrequency(value);
+    frequencyRef.current = value;
+  };
+
+  const handlePlayButtonClick = (): void => {
+    if (!audioContextRef.current) {
+      audioContextRef.current = new AudioContext();
+    }
+
+    const audioCtx = audioContextRef.current;
+
+    if (!waveRef.current) {
+      waveRef.current = audioCtx.createPeriodicWave(
+        WAVE_TABLE.real,
+        WAVE_TABLE.imag
+      );
+    }
+
+    setIsPlaying((prev) => {
+      if (!prev) {
+        if (audioCtx.state === "suspended") {
+          audioCtx.resume();
+        }
+
+        currentNoteRef.current = 0;
+        nextNoteTimeRef.current = audioCtx.currentTime;
+
+        if (!timerIDRef.current) {
+          scheduler();
+        }
+      } else {
+        if (timerIDRef.current) {
+          clearTimeout(timerIDRef.current);
+          timerIDRef.current = null;
+        }
+      }
+
+      return !prev;
+    });
+  };
+  const playButtonLabel = isPlaying ? "STOP" : "START";
+
   return (
     <main>
       <h1 className="text-2xl font-bold">Example 2. Audio Sequencer</h1>
@@ -130,77 +168,95 @@ export default function Page() {
         {/**
          * SWEEP CONTROLS
          */}
-        <section className="flex flex-col">
-          <h3>Sweep Controls</h3>
-          <div>
-            <label htmlFor="attack">Attack: {attackTime}</label>
-            <input
-              name="attack"
-              type="range"
-              min={0}
-              max={1}
-              step={0.1}
-              value={attackTime}
-              onChange={handleAttackChange}
-            />
+        <section className="flex flex-col mt-5">
+          <div className="flex space-x-5">
+            <h3 className="text-2xl">Sweep Controls</h3>
+            <div className="flex flex-col">
+              <label>
+                <p>Att</p>
+                <input
+                  name="attack"
+                  type="range"
+                  min={0}
+                  max={1}
+                  step={0.1}
+                  value={attackTime}
+                  onChange={handleAttackChange}
+                  className="ml-3"
+                />
+              </label>
 
-            <label htmlFor="release">Release: {releaseTime}</label>
-            <input
-              name="release"
-              type="range"
-              min={0}
-              max={1}
-              step={0.1}
-              value={releaseTime}
-              onChange={handleReleaseChange}
-            />
-          </div>
+              <label className="m-1">
+                <p>Rel</p>
+                <input
+                  type="range"
+                  min={0}
+                  max={2}
+                  step={0.1}
+                  value={releaseTime}
+                  onChange={handleReleaseChange}
+                />
+              </label>
 
-          <div className="flex">
-            <Pad
-              label={"1"}
-              isChecked={sweepBar[0]}
-              onChange={() => {
-                setSweepBar((prev) => {
-                  const next = [...prev];
-                  next[0] = !next[0];
-                  return next;
-                });
-              }}
-            />
-            <Pad
-              label={"2"}
-              isChecked={sweepBar[1]}
-              onChange={() => {
-                setSweepBar((prev) => {
-                  const next = [...prev];
-                  next[1] = !next[1];
-                  return next;
-                });
-              }}
-            />
-            <Pad
-              label={"3"}
-              isChecked={sweepBar[2]}
-              onChange={() => {
-                setSweepBar((prev) => {
-                  const next = [...prev];
-                  next[2] = !next[2];
-                  return next;
-                });
-              }}
-            />
-            <Pad
-              label={"4"}
-              isChecked={sweepBar[3]}
-              onChange={() => {
-                setSweepBar((prev) => {
-                  const next = [...prev];
-                  next[3] = !next[3];
-                  return next;
-                });
-              }}
-            />
+              <label>
+                <p>Frq</p>
+                <input
+                  type="range"
+                  min={0}
+                  max={400}
+                  step={10}
+                  value={frequency}
+                  onChange={handleFrequencyChange}
+                />
+              </label>
+            </div>
+
+            <div className="flex">
+              <Pad
+                label={"1"}
+                isChecked={sweepBar[0]}
+                onChange={() => {
+                  setSweepBar((prev) => {
+                    const next = [...prev];
+                    next[0] = !next[0];
+                    return next;
+                  });
+                }}
+              />
+              <Pad
+                label={"2"}
+                isChecked={sweepBar[1]}
+                onChange={() => {
+                  setSweepBar((prev) => {
+                    const next = [...prev];
+                    next[1] = !next[1];
+                    return next;
+                  });
+                }}
+              />
+              <Pad
+                label={"3"}
+                isChecked={sweepBar[2]}
+                onChange={() => {
+                  setSweepBar((prev) => {
+                    const next = [...prev];
+                    next[2] = !next[2];
+                    return next;
+                  });
+                }}
+              />
+              <Pad
+                label={"4"}
+                isChecked={sweepBar[3]}
+                onChange={() => {
+                  setSweepBar((prev) => {
+                    const next = [...prev];
+                    next[3] = !next[3];
+                    return next;
+                  });
+                }}
+              />
+            </div>
           </div>
         </section>
 
@@ -208,14 +264,13 @@ export default function Page() {
          * BPM
          */}
         <section>
-          <h3>BPM</h3>
           <div>
             <label htmlFor="bpm">BPM: {tempo}</label>
             <input
               name="bpm"
               type="range"
               min={60}
-              max={120}
+              max={200}
               step={1}
               value={tempo}
               onChange={handleBPMChange}
@@ -223,54 +278,13 @@ export default function Page() {
           </div>
         </section>
 
+        {/**Play Button */}
         <div>
-          <label>
-            <span>
-              {isPlaying ? (
-                <p className="text-red-700">STOP</p>
-              ) : (
-                <p className="text-blue-700">START</p>
-              )}
-            </span>
-            <input
-              type="button"
-              onClick={() => {
-                if (!audioContextRef.current) {
-                  audioContextRef.current = new AudioContext();
-                }
-
-                const audioCtx = audioContextRef.current;
-
-                if (!waveRef.current) {
-                  waveRef.current = audioCtx.createPeriodicWave(
-                    WAVE_TABLE.real,
-                    WAVE_TABLE.imag
-                  );
-                }
-
-                setIsPlaying((prev) => {
-                  if (!prev) {
-                    if (audioCtx.state === "suspended") {
-                      audioCtx.resume();
-                    }
-
-                    currentNoteRef.current = 0;
-                    nextNoteTimeRef.current = audioCtx.currentTime;
-
-                    if (!timerIDRef.current) {
-                      scheduler();
-                    }
-                  } else {
-                    if (timerIDRef.current) {
-                      clearTimeout(timerIDRef.current);
-                      timerIDRef.current = null;
-                    }
-                  }
-
-                  return !prev;
-                });
-              }}
-            />
+          <label className="bg-blue-500">
+            <p className={isPlaying ? "text-red-700" : "text-blue-700"}>
+              {playButtonLabel}
+            </p>
+            <input type="button" onClick={handlePlayButtonClick} />
           </label>
         </div>
       </article>
